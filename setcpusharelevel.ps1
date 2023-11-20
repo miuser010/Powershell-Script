a078c6c041c62e9bc9ba92577736fa360884b601
miuser010 <120388250+miuser010@users.noreply.github.com>
2023-11-20 16:02:30 +0100

    Add files via upload

---
+++ setcpusharelevel.ps1
@@ -0,0 +1,11 @@
+﻿# Connect to your vCenter server
+Connect-VIServer -Server czdcm-vcenter.mg.ifortuna.cz -User a_mazkha -Password 
+
+foreach($line in Get-Content .\vmlist.txt) {
+    # check
+    Get-VMResourceConfiguration -VM $line | Format-Custom -Property CpuSharesLevel
+    # change
+    Get-VM -Name $line | Get-VMResourceConfiguration | Set-VMResourceConfiguration -CpuSharesLevel High
+    # verify
+    Get-VMResourceConfiguration -VM $line | Format-Custom -Property CpuSharesLevel
+} 
---
+++ setvm.ps1
@@ -0,0 +1,25 @@
+﻿# Connect to your vCenter server
+Connect-VIServer -Server czdcm-vcenter.mg.ifortuna.cz -User a_mazkha -Password 
+
+# Specify the path to the file containing VM names
+$vmListFile = "C:\Users\mazkha\Downloads\vmlist.txt"
+
+# Read VM names from the file
+$vmNames = Get-Content $vmListFile
+
+# Loop through each VM and set CPU shares to high
+foreach ($vmName in $vmNames) {
+    $vm = Get-VM -Name $vmName
+
+    if ($vm -ne $null) {
+        # Set CPU shares to high (High = 2000)
+        $vm | Get-VMResourceConfiguration | Set-VMResourceConfiguration -CpuSharesLevel High
+
+        Write-Host "CPU shares set to high for VM: $vmName"
+    } else {
+        Write-Host "VM not found: $vmName"
+    }
+}
+
+# Disconnect from vCenter server
+Disconnect-VIServer -Confirm:$false
