31950ffd2529a0a1a72a5d347d35b8cea18285f1
miuser010 <120388250+miuser010@users.noreply.github.com>
2024-03-05 12:57:32 +0100

    Add files via upload
    Export Azure App registrations with Owners and Certificates secrets

---
+++ App_Owner_cert_expired.ps1
@@ -0,0 +1,41 @@
+﻿$Apps = Get-AzureADApplication -All $true
+$today = Get-Date
+$credentials = @()
+
+$Apps | ForEach-Object {
+    $aadAppObjId = $_.ObjectId
+    $app = Get-AzureADApplication -ObjectId $aadAppObjId
+    $owner = Get-AzureADApplicationOwner -ObjectId $aadAppObjId
+
+    $app.KeyCredentials | ForEach-Object {
+        $credentials += [PSCustomObject] @{
+            CredentialType = "KeyCredentials"
+            DisplayName = $app.DisplayName
+            AppId = $app.AppId
+            ExpiryDate = $_.EndDate
+            StartDate = $_.StartDate
+            Type = $_.Type
+            Usage = $_.Usage
+            Owners = $owner.UserPrincipalName
+            Expired = ([DateTime]$_.EndDate -lt $today)
+        }
+    }
+
+    $app.PasswordCredentials | ForEach-Object {
+        $credentials += [PSCustomObject] @{
+            CredentialType = "PasswordCredentials"
+            DisplayName = $app.DisplayName
+            AppId = $app.AppId
+            ExpiryDate = $_.EndDate
+            StartDate = $_.StartDate
+            Type = 'NA'
+            Usage = 'NA'
+            Owners = $owner.UserPrincipalName
+            Expired = ([DateTime]$_.EndDate -lt $today)
+        }
+    }
+}
+
+$credentials | Format-Table -AutoSize
+
+$credentials | Export-Csv -Path "c:\temp\AppsInventory.csv" -NoTypeInformation
